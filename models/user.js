7d5474d2e5549be7a70d25622db64d4279ab3eac
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2016-12-03 16:27:16 -0500

    modified user model file

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback) {
     bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash("newUser.password", salt, function(err, hash) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
             // Store hash in your password DB. 
             newUser.password = hash;
             newUser.save(callback);
